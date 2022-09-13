e57fe420eedc690d1ee95a5951ff7a9690af4289
NeonLife (Owner) ItssJxstn <113269208+NeonLifeRPde@users.noreply.github.com>
2022-09-13 20:20:10 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,18 @@
------------------------------
---- Taser Cartridge Limit ---
----     Made by L1 & ItssJxstn     ---
------------------------------
+-- Stating the FX Version and Game 
+fx_version 'cerulean'
+games 'gta5'
 
-client_script "client.lua"
-server_script "server.lua"
+-- Insert Author Name, Description and Version of Script
+author 'L1 & ItssJxstn
+description 'Taser Cardridge for qb-core'
+version '1.0.0'
+
+-- Insert Client Script Names
+client_scripts {
+    'client.lua'
+}
+
+-- Insert Server Script Names
+server_scripts {
+  'server.lua'
+}
