2373097375ae3f71cf1e0a56ac903bd2a318883a
NeonLife (Owner) ItssJxstn <113269208+NeonLifeRPde@users.noreply.github.com>
2022-12-21 20:22:50 +0100

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,74 +1,107 @@
---[[
-Taser cardridge
-by L1 & ItssJxstn
-]]
 local QBCore = exports['qb-core']:GetCoreObject()
-    RegisterKeyMapping("taserreload", "Reload", 'keyboard', "R")
+local OnNotify = false
+local CardridgesLeft = Config.max
+local TaserModel = GetHashKey("WEAPON_STUNGUN")
 
 
-local maxTaserCarts = 2
-local refillCommand = "taserreload"
-local longerTazeTime = false
-local longerTazeSecTime = 20
-taserModel = GetHashKey("WEAPON_STUNGUN")
-local taserCartsLeft = maxTaserCarts
-
---- Code ---
-
-InNotify = false
-
-function ShowNotification(text, time)
-    _time = time or 2000
-    CreateThread(function()
-        if(InNotify == false) then
-            QBCore.Functions.Notify(text, 'primary', _time)
-            InNotify = true
-            Wait(_time - 1)
-            InNotify = false
-
-        end
+-- Make a usable command
+if Config.type == "command" then
+    RegisterCommand(Config.command, function()
+        RefillTaser()
     end)
-
+    RegisterKeyMapping(Config.command, 'Taser nachladen', 'keyboard', 'r')
 end
 
-RegisterCommand(refillCommand, function(source, args, rawCommand)
-    if(GetSelectedPedWeapon(PlayerPedId()) == taserModel) then
-        if QBCore.Functions.HasItem('taser_cardridge') then
-            ShowNotification("Reloading...", 3000)
-            Citizen.Wait(3000)
-            taserCartsLeft = maxTaserCarts
-            ShowNotification("Taser Reloaded.")
-            TriggerServerEvent('DD-taser:server:jhkjkl')
-        else
-            ShowNotification("You need a Cardridge")
-        end
-    end
+-- event for the item
+RegisterNetEvent('qb-tasercart:server:refillTaser', function()
+    RefillTaser()
 end)
 
-Citizen.CreateThread(function()
+-- notify which is safe to spam
+-- @param msg - message to display
+-- @param time - time to display message
+function SafeNotify(msg, time)
+    CreateThread(function()
+        -- if already displaying a message, don't display another
+        if OnNotify then
+            return
+        end
+        -- if no time is specified, default to 2 seconds
+        if time == nil then
+            time = 2000
+        end
+        print(OnNotify)
+        QBCore.Functions.Notify(msg, 'primary', time - 1)
+        OnNotify = true
+        Wait(time - 1)
+        OnNotify = false
+    end)
+end
+
+-- refill taser function
+function RefillTaser()
+    if Config.progressbar then
+        RefillProgressbar()
+    else
+        RefillNotify()
+    end
+end
+
+function RefillProgressbar()
+     -- progressbar
+     if QBCore.Functions.HasItem('taser_cardridge') then
+        if QBCore.Functions.HasItem('weapon_stungun') then
+        QBCore.Functions.Progressbar("taser", "Lade taser...", Config.ReloadTime, false, true, {
+            disableMovement = false,
+            disableCarMovement = false,
+            disableMouse = false,
+            disableCombat = true
+        }, {}, {}, {}, function() -- Done
+            SafeNotify("Taser Nachgeladen.")
+            CardridgesLeft = Config.max
+            TriggerServerEvent("qb-tasercart:server:removeCardridge")
+        end, function() -- Cancel
+            SafeNotify("Abgebochen", 1000)
+        end)
+    else
+        SafeNotify("Du hast keinen Taser", 1000)
+    end
+end
+end
+
+function RefillNotify()
+    CreateThread(function()
+        if QBCore.Functions.HasItem('taser_cardridge') then
+            SafeNotify("Refilling taser...", Config.ReloadTime)
+            Wait(Config.ReloadTime)
+            SafeNotify("Taser Reloaded.")
+            TriggerServerEvent("qb-tasercart:server:removeCardridge")
+            CardridgesLeft = Config.max
+        else
+            SafeNotify("Du hast keine Kartuschen", 1000)
+        end
+    end)
+end
+
+-- loop
+CreateThread(function()
     while true do
-        Citizen.Wait(0)
+        Wait(0)
         local ped = PlayerPedId()
+        local weapon = GetSelectedPedWeapon(ped)
+        if weapon == TaserModel then
 
-        if GetSelectedPedWeapon(ped) == taserModel then
-            if IsPedShooting(ped) then
-                taserCartsLeft = taserCartsLeft - 1
-                -- print("[Taser Cartridge] Taser Cartridges over = " .. taserCartsLeft)
-            end
-        end
-
-        if taserCartsLeft <= 0 then
-            if GetSelectedPedWeapon(ped) == taserModel then
-                SetPlayerCanDoDriveBy(ped, false)
+            if CardridgesLeft <= 0 then
                 DisablePlayerFiring(ped, true)
-                if IsControlPressed(0, 106) then
-                    ShowNotification("Your Taser is Empty. Please Reload!")
-                end
             end
-        end
 
-        if longerTazeTime then
-            SetPedMinGroundTimeForStungun(ped, longerTazeSecTime * 1000)
+            if IsControlPressed(0, 106) and CardridgesLeft <= 0 then
+                SafeNotify("Dein Taser ist Leer", 1000)
+            end
+
+            if IsPedShooting(ped) and CardridgesLeft > 0 then
+                CardridgesLeft = CardridgesLeft - 1
+            end
         end
     end
 end)
